87b3a8d3ec3db3c61fda3af07dd33c2a8cfe1c40
HexTheCode <alopezgarcia380@gmail.com>
2020-10-10 16:09:16 +0200

    New Features: -ECoins Api request -Error handle

---
+++ src/components/Form.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import Error from './Error';
+import useCoin from "../hooks/useCoin";
+import useCriptoCoin from "../hooks/useCriptoCoin";
+import axios from "axios";
+
+const Button = styled.input`
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 20px;
+  padding: 10px;
+  background-color: #66a2fe;
+  border: none;
+  width: 100%;
+  border-radius: 10px;
+  color: #fff;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #326ac0;
+    cursor: pointer;
+  }
+`;
+
+const Form = () => {
+  // State del listado criptomonedas
+
+  const [listcripto, saveListCripto] = useState([]);
+  const [error, saveError] = useState(false);
+
+  const COINS = [
+    { cod: "USD", name: "Dolar Estadounidense" },
+    { cod: "MXN", name: "Peso Mexicano" },
+    { cod: "EUR", name: "Euro" },
+    { cod: "GBP", name: "Libra Esterlina" },
+  ];
+
+  const [coin, Coins, updateCoin] = useCoin("Divisas:", "", COINS);
+
+  const [ecoins, CriptoCoins] = useCriptoCoin(
+    "Elige criptomoneda",
+    "",
+    listcripto
+  );
+
+  useEffect(() => {
+    const apiRequest = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+      const endpoint = await axios.get(url);
+
+      saveListCripto(endpoint.data.Data);
+    };
+
+    apiRequest();
+  }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (coin === "" || ecoins === "") {
+      saveError(true);
+      return;
+    }
+    saveError(false);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      {error ? <Error  message={"Hay un error"}/> : null}
+      <Coins />
+      <CriptoCoins />
+      <Button type="submit" value="Calcular" />
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/hooks/useCriptoCoin.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useState } from "react";
+import styled from "@emotion/styled";
+
+const Label = styled.label`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.2rem;
+`;
+
+const useCriptoCoin = (label, startState, options) => {
+  // Hook State
+
+  const [state, updateState] = useState(startState);
+
+  const CriptoCoins = () => (
+    <Fragment>
+      <Label id="label">{label}</Label>
+      <Select
+        onChange={(e) => updateState(e.target.value)}
+        htmlFor="label"
+        value={state}
+      >
+        <option value="">-- Seleccionar --</option>
+        {options.map((option) => (
+          <option key={option.CoinInfo.Id} value={option.CoinInfo.Name}>
+            {option.CoinInfo.FullName}
+          </option>
+        ))}
+      </Select>
+    </Fragment>
+  );
+
+  // Return state, Select and updateState
+  return [state, CriptoCoins, updateState];
+};
+
+export default useCriptoCoin;
